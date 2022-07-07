8d245957770505066a68ae7c87930e558cdaa839
superstar0718 <104906327+Cheng0718@users.noreply.github.com>
2022-07-07 03:16:44 -0800

    c4

--- src/Scenes/reviewScene.js
+++ src/Scenes/reviewScene.js
@@ -21,20 +21,20 @@ let clickedList = []
 let wordGround = [6, 6, 2]
 
 const posInfoList = [
-    { x: 1, y: 40 },
+    { x: 4, y: 40 },
     { x: 34, y: 40 },
-    { x: 68, y: 40, m: true },
-    { x: 1, y: 70 },
+    { x: 64, y: 40, m: true },
+    { x: 4, y: 70 },
     { x: 34, y: 70 },
-    { x: 68, y: 70, m: true },
+    { x: 64, y: 70, m: true },
 
 
-    { x: 1, y: 40 },
+    { x: 4, y: 40 },
     { x: 34, y: 40 },
-    { x: 68, y: 40, m: true },
-    { x: 1, y: 70 },
+    { x: 64, y: 40, m: true },
+    { x: 4, y: 70 },
     { x: 34, y: 70 },
-    { x: 68, y: 70, m: true },
+    { x: 64, y: 70, m: true },
 
 
     { x: 10, y: 55 },
